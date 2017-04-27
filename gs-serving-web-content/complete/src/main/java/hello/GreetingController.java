740c0b393ad5831619764ecba5c69f54ea2bbc47
BenNBen <barriageb@gmail.com>
2017-04-27 10:27:39 -0400

    Media support

--- gs-serving-web-content/complete/src/main/java/hello/GreetingController.java
+++ gs-serving-web-content/complete/src/main/java/hello/GreetingController.java
@@ -18,13 +18,14 @@ public class GreetingController {
     @RequestMapping("/biblio")
     public String greeting(Model model) {
         List<Entry> entries = this.jdbcTemplate.query(
-        "select author, title, year, journal from entries",
+        "select author, title, year, journal, and media (movie, book, game) from entries",
         new RowMapper<Entry>() {
             public Entry mapRow(ResultSet rs, int rowNum) throws SQLException {
                 Entry entry = new Entry(rs.getString("author"),
                                         rs.getString("title"),
                                         rs.getInt("year"),
-                                        rs.getString("journal")
+                                        rs.getString("journal"),
+					rs.getString("media")
                                         );
                 return entry;
             }
@@ -35,11 +36,12 @@ public class GreetingController {
 
     @RequestMapping("/addEntry")
     public String addEntry(@RequestParam(value="author", required=true) String a,
-            @RequestParam(value="title", required=true) String t,
-            @RequestParam(value="year", required=true) int y,
-            @RequestParam(value="journal", required=true) String j,
+			   @RequestParam(value="title", required=true) String t,
+			   @RequestParam(value="year", required=true) int y,
+			   @RequestParam(value="journal", required=true) String j,
+			   @RequestParam(value="media", required=true) String m,
             Model model) {
-        jdbcTemplate.update("insert into entries values (?, ?, ?, ?)", a, t, y, j);
+        jdbcTemplate.update("insert into entries values (?, ?, ?, ?)", a, t, y, j,m);
         return "redirect:biblio"; // back to the biblio view
     }
 
@@ -51,12 +53,14 @@ public class GreetingController {
         final public String title;
         final public int year;
         final public String journal;
+	final public String media;
 
-        Entry(String a, String t, int y, String j) {
+        Entry(String a, String t, int y, String j, String m) {
             author = a;
             title = t;
             year = y;
             journal = j;
+	    media = m;
         }
     }
 }
