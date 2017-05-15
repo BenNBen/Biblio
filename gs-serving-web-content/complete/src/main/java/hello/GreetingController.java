d6287091d5bcdd00f54588d1d8343ce14052f29a
BenNBen <barriageb@gmail.com>
2017-05-14 22:29:37 -0400

    Added Comments

--- gs-serving-web-content/complete/src/main/java/hello/GreetingController.java
+++ gs-serving-web-content/complete/src/main/java/hello/GreetingController.java
@@ -1,5 +1,9 @@
 package hello;
-
+/*
+  Authors: Ben Barriage & Anne Marie Bogar
+  Purpose: Design an HTML/Java/MYSQL based Bibliography program supporting multiple types of media (book, film, journal, lecture, website)
+  Date: 5/10/17
+ */
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
@@ -26,7 +30,7 @@ public class GreetingController {
     private List<Entry> entries;
     @GetMapping("/biblio")
     public String greeting(Model model) {
-	
+	//establishing new entries of each type
 	List<Entry> films = this.jdbcTemplate.query(
 						    "select title, directorFN, directorLN, actors, studio, medium, year from films",
 						    new RowMapper<Entry>() {
@@ -114,6 +118,7 @@ public class GreetingController {
 							       return entry;
 							   }
 						       });
+	//display all list entries of each media type
 	List<Entry> entries = new ArrayList<Entry>(films);
 	entries.addAll(books);
 	entries.addAll(journals);
@@ -136,7 +141,8 @@ public class GreetingController {
 	this.entries = entries;
 	return "biblio";
     }
-    
+
+    //used to delete an entry from the table using the last name of the author and the title of the work, update once done
     @RequestMapping("/deleteEntry")
     public String deleteEntry(@RequestParam(value="authorLN") String authorLN, @RequestParam(value="title") String title, @RequestParam(value="type") String type, Model model) {
 	for(Entry entry : this.entries){
@@ -162,41 +168,49 @@ public class GreetingController {
 	}
 	return "redirect:biblio"; // back to the biblio view
     }
- 
+
+
+    //used to switch to alternate page to add information into sql
     @GetMapping("/addWebsite")
     public String switchWebsite(Model model){
 	return "addWebsite";
     }
 
-    @RequestMapping("/addWebsite")
+    //the actual page/function call to add information to the MySQL table
+    @PostMapping("/addWebsite")
     public String addWebsite(@RequestParam(value="article", required=true) String article,
 			     @RequestParam(value="authorFN", required=true) String authorFN,
 			     @RequestParam(value="authorLN", required=true) String authorLN,
 			     @RequestParam(value="website", required=true) String website,
-			     @RequestParam(value="publisher", required=true) String publisher,
+			     @RequestParam(value="publisher", required=false) String publisher,
 			     @RequestParam(value="url", required=true) String url,
-			     @RequestParam(value="publishDate", required=true) String publishDate,
+			     @RequestParam(value="publishDate", required=false) String publishDate,
 			     @RequestParam(value="accessDate", required=true) String accessDate,
 			     Model model) {
-		if(publishDate.equals("")){
+	//this is because not every website will have a publishDate, nor publisher
+	if(publishDate.equals("")){
 	    publishDate = "N/A";
 	}
 	if(publisher.equals("")){
 	    publisher = "N/A";
 	}
+
+	//used to check if that item already exists, if so the item is not added
 	if(detectDuplicate(article, authorLN, "website") == false){
 	    jdbcTemplate.update("insert into websites values (?, ?, ?, ?, ?, ?, ?, ?)", article, authorFN, authorLN, website, publisher, url, publishDate, accessDate);
 	    }
 	return "redirect:biblio"; // back to the biblio view
 	
     }
-    
+
+    //function used to switch view to that of adding a book
     @GetMapping("/addBook")
     public String switchBook(Model model){
 	return "addBook";
     }
 
-    @RequestMapping("/addBook")
+    //actual function used to submit book information for a new database entry
+    @PostMapping("/addBook")
     public String addBook(@RequestParam(value="title", required=true) String title,
 			  @RequestParam(value="authorFN", required=true) String authorFN,
 			  @RequestParam(value="authorLN", required=true) String authorLN,
@@ -206,50 +220,57 @@ public class GreetingController {
 			  @RequestParam(value="city", required=true) String city,
 			  @RequestParam(value="year", required=true) String year,
 			  Model model) {
+	//checks if the entered information matches that of a current entry
 	if(detectDuplicate(title, authorLN, "book") == false){
 	    jdbcTemplate.update("insert into books values (?, ?, ?, ?, ?, ?, ?, ?)", title, authorFN, authorLN, volume, publisher, edition, year, city);
 	    }
 	return "redirect:biblio"; // back to the biblio view
     }
-    
+
+    //function used to switch view to that of adding journal information
     @GetMapping("/addJournal")
     public String switchJournal(Model model){
 	return "addJournal";
     }
 
-    
-    @RequestMapping("/addJournal")
+    //function used to add journal information into the database from the webpage
+    @PostMapping("/addJournal")
     public String addJournal(@RequestParam(value="article", required=true) String article,
 			     @RequestParam(value="authorFN", required=true) String authorFN,
 			     @RequestParam(value="authorLN", required=true) String authorLN,
 			     @RequestParam(value="journal", required=true) String journal,
-			     @RequestParam(value="volume", required=true) String volume,
-			     @RequestParam(value="issue", required=true) String issue,
+			     @RequestParam(value="volume", required=false) String volume,
+			     @RequestParam(value="issue", required=false) String issue,
 			     @RequestParam(value="year", required=true) String year,
 			     @RequestParam(value="pageStart", required=true) String pageStart,
 			     @RequestParam(value="pageEnd", required=true) String pageEnd,
 			     @RequestParam(value="database", required=true) String dBase,
 			     @RequestParam(value="accessDate", required=true) String accessDate,
 			     Model model) {
+
+	//not all journals will have a volume or issue, so they are not required
 	if(volume.equals("")){
 	    volume = "N/A";
 	}
 	if(issue.equals("")){
 	    issue = "N/A";
 	}
+	//checks if entered information matches that of a current database entry
 	if(detectDuplicate(article, authorLN, "journal") == false){
 	    jdbcTemplate.update("insert into journals values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", article, authorFN, authorLN, volume, journal, issue, year, pageStart, pageEnd, dBase, accessDate);
 	}
 	
 	return "redirect:biblio"; // back to the biblio view
     }
-    
+
+    //switches view to that of adding a film
     @GetMapping("/addFilm")
     public String switchFilm(Model model) {
 	return "addFilm"; // back to the biblio view
     }
 
-    @RequestMapping("/addFilm")
+    //adds entered film data into the MySQL database
+    @PostMapping("/addFilm")
     public String addFilm(
 			  @RequestParam(value="title", required=true) String title,
 			  @RequestParam(value="directorFN", required=true) String directorFN,
@@ -259,6 +280,7 @@ public class GreetingController {
 			  @RequestParam(value="medium", required=true) String medium,
 			  @RequestParam(value="year", required=true) String year,
 			  Model model){
+	//checks if entered film data already belongs to the MySQL database, only adds entry if it does not currently exist
 	if(detectDuplicate(title, directorLN, "film") == false){
 	    jdbcTemplate.update("insert into films values (?, ?, ?, ?, ?, ?, ?)", title, directorFN, directorLN, actors, studio, medium, year);
 	}
@@ -266,13 +288,14 @@ public class GreetingController {
     }
     
 
-    
+    //function used to switch view to that of adding a lecture citation
     @GetMapping("/addLecture")
     public String switchLecture(Model model){
 	return "addLecture";
     }
 
-    @RequestMapping("/addLecture")
+    //function used to enter lecture information into the MySQL database
+    @PostMapping("/addLecture")
     public String addLecture(@RequestParam(value="presentation", required=true) String presentation,
 			     @RequestParam(value="speakerFN", required=true) String speakerFN,
 			     @RequestParam(value="speakerLN",required=true) String speakerLN,
@@ -282,12 +305,14 @@ public class GreetingController {
 			     @RequestParam(value="location", required=true) String location,
 			     @RequestParam(value="date", required=true) String date,
 			     Model model) {
+	//checks if the entered information already exists in the database based off of presentation and speaker Last name; if so, the data is not added
 	if(detectDuplicate(presentation, speakerLN, "lecture") == false){
 	    jdbcTemplate.update("insert into lectures values (?, ?, ?, ?, ?, ?, ?, ?)", presentation, speakerFN, speakerLN, type, event, city, location, date);
 	}    
 	return "redirect:biblio";
     }
-    
+
+    //function used to check whether a proposed entry matches that of an entry currently in the database. used to keep duplicate information from being added into the database
     public boolean detectDuplicate(String title, String lastName, String type){
 	for(Entry entry : this.entries){
 	    if(entry.title.compareTo(title) == 0 && entry.lastName.compareTo(lastName) == 0){
@@ -312,7 +337,8 @@ public class GreetingController {
 	}
 	return false;
     }
-    
+
+    //search entry function used to check whether an entry currently exists in the database based off of the entered last name and work, as well as type of media
     @RequestMapping("/searchEntry")
     public String search(@RequestParam(value="lastName", required=true) String a, @RequestParam(value="work", required=true) String t, @RequestParam(value="type", required=true) String type, Model model) {
 	for(Entry entry : this.entries){
